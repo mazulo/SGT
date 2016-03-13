68fd20513bfc9ba5d1de821d671755bfafd08f02
Patrick Mazulo <pmazulo@gmail.com>
2016-03-12 21:38:53 -0800

    Create input/output values model

--- sgt/cash_register/admin.py
+++ sgt/cash_register/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CashInput, CashOutput
+
+
+admin.site.register(CashInput)
+admin.site.register(CashOutput)
--- sgt/cash_register/models.py
+++ sgt/cash_register/models.py
@@ -1,6 +1,70 @@
 from django.db import models
 
 
-class CashRegister(models.Model):
+class CashValue(models.Model):
 
-    pass
+    TYPES = (
+        (
+            ('M', 'Mensalidade'),
+            ('D', 'Doações'),
+            ('A', 'Entrada Avulsa'),
+        ),
+        (
+            ('C', 'Compra de Materiais'),
+            ('S', 'Pagamento Seguro'),
+        )
+    )
+
+    cash_value = models.DecimalField(
+        'Valor',
+        max_digits=8,
+        decimal_places=2
+    )
+    date_value = models.DateField('Data', auto_now_add=True)
+    # type_
+    description = models.TextField('Descrição', blank=True)
+    cash_flow = models.ForeignKey(
+        'CashFlow',
+        verbose_name='Caixa',
+        related_name="%(app_label)s_%(class)s_flow"
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        self.cash_value
+
+
+class CashInput(CashValue):
+
+    _type = models.CharField('Tipo de Entrada', choices=CashValue.TYPES[0])
+
+    class Meta:
+        verbose_name = "Entrada de Dinheiro"
+        verbose_name_plural = "Entradas de Dinheiro"
+
+
+class CashOutput(CashValue):
+
+    _type = models.CharField('Tipo de Entrada', choices=CashValue.TYPES[1])
+
+    class Meta:
+        verbose_name = "Saída de Dinheiro"
+        verbose_name_plural = "Saídas de Dinheiro"
+
+
+class CashFlow(models.Model):
+
+    total_values = models.DecimalField(
+        'Valor',
+        max_digits=8,
+        decimal_places=2
+    )
+
+    class Meta:
+        verbose_name = "Fluxo de Caixa"
+        verbose_name_plural = "Fluxos de Caixa"
+
+    def __str__(self):
+        pass
